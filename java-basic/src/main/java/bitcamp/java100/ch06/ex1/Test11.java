70f2d6de1bf0dede8ec93f8325d19a5ff0855d53
lee bomi <bom2zzang@me.com>
2017-10-20 16:56:10 +0900

    good

---
+++ java-basic/src/main/java/bitcamp/java100/ch06/ex1/Test11.java
@@ -0,0 +1,23 @@
+package bitcamp.java100.ch06.ex1;
+
+//사용자정의데이터타입
+public class Test11 {
+    public static void main(String[] args) {
+        
+        Score10 score;
+        score = new Score10();
+        
+
+        System.out.printf("이름=%s\n",score.name);
+        System.out.printf("총점=%s\n",score.sum);
+        System.out.printf("평균=%s\n",score.aver);
+        
+        
+        
+    
+    }
+    
+    
+    
+
+}
